7f9004a42bdccd0aaaf50f7a9e353bc646f6b9d9
Velkow <sivade.g@gmail.com>
2020-03-09 21:57:31 +0100

    fix on mobile resolution nav not clickable

--- templates/nav.php
+++ templates/nav.php
@@ -1,26 +1,28 @@
-<div id="nav" class="twelve columns">
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <?php
-        if (isset($_SESSION["username"])) {
-        ?>
+<div class="row">
+    <div id="nav" class="twelve columns">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
             <?php
-            if (SecurityService::requiredRank(RanksEnum::WRITER)) {
-            ?>
-                <li><a href="/write">Write</a></li>
-            <?php
+            if (isset($_SESSION["username"])) {
+                ?>
+                <?php
+                if (SecurityService::requiredRank(RanksEnum::WRITER)) {
+                    ?>
+                    <li><a href="/write">Write</a></li>
+                    <?php
+                }
+                ?>
+                <li><a href="/profile"><?= $_SESSION["username"] ?></a></li>
+                <li><a href="/logout">Logout</a></li>
+                <?php
+            } else {
+                ?>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Register</a></li>
+                <?php
             }
             ?>
-            <li><a href="/profile"><?= $_SESSION["username"] ?></a></li>
-            <li><a href="/logout">Logout</a></li>
-        <?php
-        } else {
-        ?>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/register">Register</a></li>
-        <?php
-        }
-        ?>
-    </ul>
+        </ul>
+    </div>
 </div>
